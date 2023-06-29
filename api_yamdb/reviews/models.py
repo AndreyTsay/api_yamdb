631a5cc2fc8f1c35e8f2241230a22b2538c6764a
Ян Свиридов <lansviridovlansviridov@yandex.ru>
2023-06-29 14:38:09 +0300

    Модели, вью, сериализаторы для Title, Genre, Category

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+
+class Tittle(models.Model):
+    name = models.CharField(
+        'название',
+        max_length=200,
+        db_index=True
+    )
+    year = models.IntegerField
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='категория',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        'Genre',
+        related_name='titles',
+        verbose_name='жанр'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(
+        unique=True, db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(
+        unique=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
