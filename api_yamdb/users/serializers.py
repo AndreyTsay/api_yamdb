da47750f5f8be8a78e16a014fa228e28707eb9d5
Андрей Цай <skaeris@yandex.ru>
2023-07-10 03:02:50 +0300

    MVP

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -4,11 +4,20 @@ from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
 from rest_framework_simplejwt.tokens import RefreshToken
 from users.models import User
+from .validators import validate_me
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=150)
-    email = serializers.EmailField(max_length=254)
+    username = serializers.CharField(max_length=150, required=True,
+                                     validators=[
+                                         validate_me,
+                                         UniqueValidator(queryset=User.objects.all()),
+                                         ])
+
+    email = serializers.EmailField(max_length=254, required=True,
+                                   validators=[UniqueValidator]
+                                   )
 
     class Meta:
         model = User
@@ -25,6 +34,13 @@ class UserSerializer(serializers.ModelSerializer):
         if 'username' in validated_data:
             if not re.match(r'[\w.@+-]+\Z', validated_data['username']):
                 raise serializers.ValidationError('Такой username запрещен.')
+        return validated_data
+    
+    def validate_email(self, validated_data):
+        if User.objects.filter(email=validated_data).exists():
+            raise serializers.ValidationError(
+                'Аккаунт с таким email уже существует.'
+            )
         return validated_data
 
 
