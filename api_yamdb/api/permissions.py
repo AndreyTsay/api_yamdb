c8be951449ce2c074150d2e7fb5771d7f4d8a8ee
Андрей Цай <skaeris@yandex.ru>
2023-07-12 19:51:27 +0300

    Rewiews fix 3

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -21,12 +21,9 @@ class IsSuperUserOrIsAdminOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            and (request.user.is_superuser
-                 or request.user.is_staff
-                 or request.user.is_admin)
-        )
+        return (request.user.is_superuser
+                or request.user.is_staff
+                or request.user.is_admin)
 
 
 class IsSuperUserIsAdminIsModeratorIsAuthor(permissions.BasePermission):
@@ -40,12 +37,11 @@ class IsSuperUserIsAdminIsModeratorIsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-            and (request.user.is_superuser
-                 or request.user.is_staff
-                 or request.user.is_admin
-                 or request.user.is_moderator
-                 or request.user == obj.author)
+            or (request.user.is_superuser
+                or request.user.is_staff
+                or request.user.is_admin
+                or request.user.is_moderator
+                or request.user == obj.author)
         )
 
 
